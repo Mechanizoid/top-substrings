0122c8d6854d4034bf0b64c1c4949ea411a3f338
Christopher Phoenix <hello@chrisphoenix.dev>
2026-08-27 10:12:46 -0700

    feat: implement substrings method
    Count the occurrences of substrings within a provided string and provide
    that information to the caller as a hash.

--- substrings.rb
+++ substrings.rb
@@ -3,5 +3,21 @@
 # TODO: Implement the #substrings method
 
 def substrings(string, dictionary)
+  dictionary.each_with_object(Hash.new) do | (substring, index), substrings_found |
+    occurances = string.scan(/#{Regexp.escape(substring)}/i).length
 
+    if occurances > 0
+      substrings_found[substring] = occurances
+    end
+  end
+end
+
+if __FILE__ == $0
+  print "Enter a list of valid substrings, separated by whitespace only: "
+  dictionary = gets.chomp.split
+
+  print "Enter a word or sentence: "
+  string = gets.chomp
+
+  puts substrings(string, dictionary)
 end
